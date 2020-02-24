29b44c35a495ca1abf25c116d8a552cf7a1b056c
chuvak393 <chuvak393@gmail.com>
2020-02-24 20:26:23 +0400

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,66 @@
 module.exports = function toReadable (number) {
-  
+    let num = {
+        0: 'zero',
+        1: 'one',
+        2: 'two',
+        3: 'three',
+        4: 'four',
+        5: 'five',
+        6: 'six',
+        7: 'seven',
+        8: 'eight',
+        9: 'nine',
+        10: 'ten',
+        11: 'eleven',
+        12: 'twelve',
+        13: 'thirteen',
+        14: 'fourteen',
+        15: 'fifteen',
+        16: 'sixteen',
+        17: 'seventeen',
+        18: 'eighteen',
+        19: 'nineteen',
+        20: 'twenty',
+        30: 'thirty',
+        40: 'forty',
+        50: 'fifty',
+        60: 'sixty',
+        70: 'seventy',
+        80: 'eighty',
+        90: 'ninety'
+      };
+
+      // numbers before 20
+      if (number <= 20){
+          return num[number]
+      }
+
+      // numbers before 100
+      if (number < 100){
+            if (number%10 == 0) {
+                return (num[number])
+            }
+            else {  
+        return (num[Math.floor(number/10)*10] + ' ' + num[number%10])
+                 }
+    }
+
+      //numbers before 1000
+      if (number >= 100 && number < 1000) {
+          if (number % 100 == 0) {
+              return (num[number/100] + ' ' + 'hundred')
+          }
+
+          else {
+              if (number%100 < 20) {
+                return (num[Math.floor(number/100)] + ' hundred ' + num[number%100])  
+              }
+              if (number%100%10 == 0) {
+                  return (num[Math.floor(number/100)] + ' hundred ' + num[number%100])
+              }
+              else {
+                  return(num[Math.floor(number/100)] + ' hundred ' + num[(number%100) - (number%10)] + ' ' + num[(number%10)])
+              }
+          }
+      }
 }
